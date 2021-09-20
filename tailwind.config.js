5522882ebce76d5b1e5e82be0681d9c0a2579b96
Franklin Shera <fshera96@gmail.com>
2021-09-20 06:37:20 +0300

    SINGLE PRODUCT VIEW ANIMATIONS

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,28 @@ module.exports = {
                     4: "#677279",
                 },
             },
+            animation: {
+                wiggle: 'wiggle 1s ease-in-out infinite',
+                cart: 'cart 5s ease-in-out infinite',
+               },
+            keyframes: {
+
+                wiggle: {
+       
+                  '0%, 100%': { transform: 'rotate(-3deg)' },
+       
+                  '50%': { transform: 'rotate(3deg)' },
+       
+                },
+
+                cart: {
+       
+                  '0%': { transform: 'translateX(-600%)' , opacity: '0' },
+                  '5%': { transform: 'translateX(-450%)' , opacity: '0.5' },       
+                  '25%': {  transform: 'translateX(30%)' , opacity: '1' },
+       
+                }
+               }
         },
 
         plugins: [
@@ -29,6 +51,7 @@ module.exports = {
         ],
     },
     variants: {
-        transitionProperty: ['responsive', 'motion-safe', 'motion-reduce']
+        transitionProperty: ['responsive', 'motion-safe', 'motion-reduce'], 
+        animation: ['hover', 'focus'],
       },
 };
